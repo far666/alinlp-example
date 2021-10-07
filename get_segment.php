e4855f3e1a665c3e6a8e6fba8abb26d12780dabf
ryan <nrs666666@gmail.com>
2021-10-07 19:00:54 +0800

    remove useless code

--- get_segment.php
+++ get_segment.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * https://help.aliyun.com/document_detail/181284.html
  */
@@ -8,9 +7,9 @@ require __DIR__ . '/vendor/autoload.php';
 use AlibabaCloud\Client\AlibabaCloud;
 
 $config = json_decode(file_get_contents("./ali.conf"), true);
-$accessKeyId = $config["accessKeyId"];
-$accessKeySecret = $config;
-AlibabaCloud::accessKeyClient($config["accessKeyId"], $config["accessKeySecret"])->regionId("cn-hangzhou")->asDefaultClient();
+AlibabaCloud::accessKeyClient($config["accessKeyId"], $config["accessKeySecret"])
+    ->regionId("cn-hangzhou")
+    ->asDefaultClient();
 
 try {
     $title = $argv[1] ?: "測試字串";
@@ -18,8 +17,8 @@ try {
     $request->withText($title)
             ->withServiceCode("alinlp")
             ->withTokenizerId("GENERAL_CHN")
-            ->withOutType(1)
-        ;
+            ->withOutType(1);
+
     $result = $request->request();
     $json = $result->all()["Data"];
     print_r($json);
